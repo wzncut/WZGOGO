f27ec5a5bf0498ced85e16a092a62c8c2073adca
ZS-PC <2240346522@qq.com>
2019-05-13 10:15:52 +0800

    test2

--- src/Scene/Home/HomeScene.js
+++ src/Scene/Home/HomeScene.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react'
-import {Platform,Animated,StyleSheet, Text, View,ScrollView,TouchableOpacity,Modal,TouchableWithoutFeedback,UIManager,findNodeHandle} from 'react-native'
+import React, { Component } from 'react'
+import { Platform, Animated, StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, TouchableWithoutFeedback, UIManager, findNodeHandle } from 'react-native'
 import theme from '../../widget/theme'
 import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons'
 import AntDesign from 'react-native-vector-icons/AntDesign'
@@ -15,116 +15,116 @@ import dgram from 'react-native-udp'
 
 
 var Dimensions = require('Dimensions');
-const {width,height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 type Props = {};
 export default class HomeScene extends Component<Props> {
 
-// static navigationOptions=()=>({
-//     headerTitle:'设备'
-// })
+  // static navigationOptions=()=>({
+  //     headerTitle:'设备'
+  // })
 
-constructor(Props){
-  super(Props)
+  constructor(Props) {
+    super(Props)
 
-  this.state = {
-    msg:'',
-    mess: '',
-    port: '',
-    ip: '',
-    serverFlag: true,
-    ShowModal1:false,
-    x:0,
-    y:0,
-    shows:'none',
-    showf:'none',
+    this.state = {
+      msg: '',
+      mess: '',
+      port: '',
+      ip: '',
+      serverFlag: true,
+      ShowModal1: false,
+      x: 0,
+      y: 0,
+      shows: 'none',
+      showf: 'none',
+    }
   }
-}
 
-_shows = () => {
-  if(this.state.shows=='none'){
-  this.setState({
-    shows:'flex'
-  })
-}
-else{
-  this.setState({
-    shows:'none'
-  })
-}
-}
+  _shows = () => {
+    if (this.state.shows == 'none') {
+      this.setState({
+        shows: 'flex'
+      })
+    }
+    else {
+      this.setState({
+        shows: 'none'
+      })
+    }
+  }
 
-_showf = () => {
-  if(this.state.showf=='none'){
-  this.setState({
-    showf:'flex'
-  })
-}
-else{
-  this.setState({
-    showf:'none'
-  })
-}
-}
+  _showf = () => {
+    if (this.state.showf == 'none') {
+      this.setState({
+        showf: 'flex'
+      })
+    }
+    else {
+      this.setState({
+        showf: 'none'
+      })
+    }
+  }
 
 
-handleClick = () => {
-	UIManager.measure(findNodeHandle(this.buttonRef),(x,y,width,height,pageX,pageY)=>{
-    // todo  
-    if(this.state.ShowModal1==false){
-    this.setState({
-      ShowModal1:true,
-      x:pageX,
-      y:pageY
+  handleClick = () => {
+    UIManager.measure(findNodeHandle(this.buttonRef), (x, y, width, height, pageX, pageY) => {
+      // todo  
+      if (this.state.ShowModal1 == false) {
+        this.setState({
+          ShowModal1: true,
+          x: pageX,
+          y: pageY
+        })
+      }
     })
   }
- })
-}
 
-_onShow(evt){
-  console.log("---------pagex",evt.nativeEvent.pageX)
-  console.log("---------pagex",evt.nativeEvent.pageY)
-  if(this.state.ShowModal1==false){
-    this.setState({
-      ShowModal1:true,
-      x:evt.nativeEvent.pageX,
-      y:evt.nativeEvent.pageY+15
-    })
+  _onShow(evt) {
+    console.log("---------pagex", evt.nativeEvent.pageX)
+    console.log("---------pagex", evt.nativeEvent.pageY)
+    if (this.state.ShowModal1 == false) {
+      this.setState({
+        ShowModal1: true,
+        x: evt.nativeEvent.pageX,
+        y: evt.nativeEvent.pageY + 15
+      })
+    }
   }
-}
 
-toByteArray = (obj) => {
-  const uint = new Uint8Array(obj.length);
-  for (var i = 0, l = obj.length; i < l; i++) {
+  toByteArray = (obj) => {
+    const uint = new Uint8Array(obj.length);
+    for (var i = 0, l = obj.length; i < l; i++) {
       uint[i] = obj.charCodeAt(i);
+    }
+    return new Uint8Array(uint);
   }
-  return new Uint8Array(uint);
-}
 
 
-SendUDP = (port, ip, mess) => {
-            let socket = dgram.createSocket('udp4')
-            socket.bind(parseInt('8889'))
-            socket.once('listening', () => {
-                const buf = this.toByteArray(mess) 
-                socket.send(buf, 0, buf.length, parseInt(port), `${ip}`, (err) => {
-                    if (err) throw err
-                    console.log('message was sent') 
-                    socket.on('message', function(msg, rinfo) {
-                      console.log('receiving') 
-                      var str= String.fromCharCode.apply(null, new Uint8Array(msg))
-                      console.log('message was received', str)
-                  //     this.setState({
-                  //     msg:msg
-                  // })
-                })
-                    // socket.close()
-                })
+  SendUDP = (port, ip, mess) => {
+    let socket = dgram.createSocket('udp4')
+    socket.bind(parseInt('8889'))
+    socket.once('listening', () => {
+      const buf = this.toByteArray(mess)
+      socket.send(buf, 0, buf.length, parseInt(port), `${ip}`, (err) => {
+        if (err) throw err
+        console.log('message was sent')
+        socket.on('message', function (msg, rinfo) {
+          console.log('receiving')
+          var str = String.fromCharCode.apply(null, new Uint8Array(msg))
+          console.log('message was received', str)
+          //     this.setState({
+          //     msg:msg
+          // })
+        })
+        // socket.close()
+      })
 
 
-            })
-          
-// console.log(this.state.ip + this.state.mess + this.state.port)
-}
+    })
+
+    // console.log(this.state.ip + this.state.mess + this.state.port)
+  }
 
 
 
@@ -132,79 +132,79 @@ SendUDP = (port, ip, mess) => {
   render() {
     return (
       <View style={styles.container}>
-           <View style={styles.TopBar}>
-              <Text  style={{flex:1,fontSize:20 ,color:'black' ,textAlign:'center'}}>设备信息</Text>
-              <AntDesign 
-              name={'search1'}
-              size={16}
-              color={'#999'}
-              />
-              </View>
-            <View style={styles.MainBar}>
-              <ScrollView>
-                <View>
-                  <TouchableOpacity style={styles.scrollItems} onPress={() =>this.SendUDP('4567', '192.168.1.110','0D wm 123456 searchall')} >
-                   <Text style={styles.Text}>主卧</Text>
-                  </TouchableOpacity >
-                </View>
-
-                <View style={{height:1,color:'black'}}></View>
-
-                <View>
-                  <TouchableOpacity  ref={(ref)=>this.buttonRef=ref} style={styles.scrollItems}  onPress={()=>{this._showf()}}>
-                    <Text style={styles.Text}>次卧</Text>
-                  </TouchableOpacity>
-                </View>
-
-                <View display={this.state.showf}>
-                  <Text>1111</Text>
-                </View>
-
-                <View style={{height:1,color:'black'}}></View>
-
-                <View>
-                  <TouchableOpacity style={styles.scrollItems} onPress={()=>{this._shows()}}>
-                    <Text style={styles.Text}>阳台</Text>
-                  </TouchableOpacity>
-                </View>
-
-                <View display={this.state.shows}>
-                  <Text>121</Text>
-                </View>
-              </ScrollView>
+        <View style={styles.TopBar}>
+          <Text style={{ flex: 1, fontSize: 20, color: 'black', textAlign: 'center' }}>设备信息</Text>
+          <AntDesign
+            name={'search1'}
+            size={16}
+            color={'#999'}
+          />
+        </View>
+        <View style={styles.MainBar}>
+          <ScrollView>
+            <View>
+              <TouchableOpacity style={styles.scrollItems} onPress={() => this.SendUDP('4567', '192.168.1.110', '0D wm 123456 searchall')} >
+                <Text style={styles.Text}>主卧</Text>
+              </TouchableOpacity >
             </View>
+
+            <View style={{ height: 1, color: 'black' }}></View>
+
+            <View>
+              <TouchableOpacity ref={(ref) => this.buttonRef = ref} style={styles.scrollItems} onPress={() => { this._showf() }}>
+                <Text style={styles.Text}>次卧</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View display={this.state.showf}>
+              <Text>1111</Text>
+            </View>
+
+            <View style={{ height: 1, color: 'black' }}></View>
+
+            <View>
+              <TouchableOpacity style={styles.scrollItems} onPress={() => { this._shows() }}>
+                <Text style={styles.Text}>阳台</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View display={this.state.shows}>
+              <Text>121</Text>
+            </View>
+          </ScrollView>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:theme.lightGray
+  container: {
+    flex: 1,
+    backgroundColor: theme.lightGray
   },
-  TopBar:{
-    height:40,
-    backgroundColor:'#F5FCFF',
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'row'
+  TopBar: {
+    height: 40,
+    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
   },
-  MainBar:{
-    height:500,
-    backgroundColor:'#9999'
+  MainBar: {
+    height: 500,
+    backgroundColor: '#9999'
   },
-  scrollItems:{
-    backgroundColor:'white',
-    flexDirection:'row',
-    height:40,
-    alignItems:'center',
-    justifyContent:'center'
+  scrollItems: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  Text:{
-    flex:1,
-    color:'#333333',
-    fontSize:16,
-    marginLeft:width*0.03
+  Text: {
+    flex: 1,
+    color: '#333333',
+    fontSize: 16,
+    marginLeft: width * 0.03
   }
 });
